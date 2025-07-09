5a109a05b9dd1bc58bd7140ce24531fada6361ec
jayallendotnet <jayallen.net@gmail.com>
2025-07-10 02:15:37 +0530

    Corrected MapController Script

--- Assets/Scripts/Controllers/MapController.cs
+++ Assets/Scripts/Controllers/MapController.cs
@@ -1,46 +1,39 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 using DG.Tweening;
-using UnityEngine.SceneManagement;
-using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 namespace BeachHero
 {
-    public class MapController : SingleTon<MapController>
+    public class MapController : MonoBehaviour
     {
+        public static MapController GetInstance { get; private set; }
+
         private static readonly Vector2 originalTextureScale = new Vector2(1, 1);
         private static readonly Vector2 zoomInTextureScale = new Vector2(3, 1);
 
         private static readonly float zoomInThick = 0.05f;
         private static readonly float zoomOutThick = 0.35f;
 
-
-        public bool validate;
+        [SerializeField] private Transform boat;
         [SerializeField] private LineRenderer pathLine;
         [SerializeField] private GameObject zoomOutCam, zoomInCam;
-        [SerializeField] private InputManager inputManager;
+        [SerializeField] private GameObject levelPrefab; // A prefab with SpriteRenderer + optional 3D stuff
+        [SerializeField] private Transform levelsParent;
 
-        Vector3 originalCameraPosition;
+        [SerializeField] private LevelDatabaseSO levelDatabase;
+        [SerializeField] private List<BezierPoint> bezierPoints = new List<BezierPoint>();
+        [SerializeField] private List<LevelVisual> levelVisuals;
+        [SerializeField] private float boatDuration = 1f;
+        [SerializeField] private float boatOffset = 0.5f;
 
+        private Tween boatTween;
+        private Vector2 pendingClickPosition;
+        private bool shouldCheckClick;
 
-
-
-        public LevelDatabaseSO levelDatabase;
-        public GameObject levelPrefab; // A prefab with SpriteRenderer + optional 3D stuff
-
-
-        public List<BezierPoint> bezierPoints = new List<BezierPoint>();
-
-        public Transform levelsParent;
-        public List<LevelVisual> levelVisuals;
+#if UNITY_EDITOR
+        public bool validate;
         public MapEditor mapEditor;
-
-        [Header("UI")]
-        [SerializeField] private Toggle zoomToggle;
-        [SerializeField] private Button mapExitBtn;
-
         private void OnValidate()
         {
             if (!validate)
@@ -78,55 +71,88 @@ namespace BeachHero
             //     tr.up = forward;
             // }
         }
+#endif
 
-        private void OnDrawGizmos()
+        #region Unity Methods
+        private void Awake()
         {
-        }
-
-        private void Start()
-        {
+            if (GetInstance == null)
+            {
+                GetInstance = this;
+            }
             for (var i = 0; i < levelVisuals.Count; i++)
             {
                 levelVisuals[i].Setup(levelDatabase.LevelDatas[i]);
             }
-            inputManager.OnEscapePressed += ZoomOut;
             DOTween.Init();
-            ZoomOut();
-            zoomToggle.onValueChanged.AddListener(ZoomToggle);
-            zoomToggle.isOn = true;
-            mapExitBtn.ButtonRegister(MapExit);
+            ZoomIn();
         }
-
-        private void MapExit()
+        private void OnEnable()
         {
-            var loading = SceneManager.LoadSceneAsync(StringUtils.GAME_SCENE); //, LoadSceneMode.Additive);
-            loading.completed += (x) =>
-                {
-                    Scene loadedScene = SceneManager.GetSceneByName(StringUtils.GAME_SCENE);
-                };
-        }
-
-        public Transform boat;
-
-        public bool move;
-
-        public void Update()
-        {
-            if (move)
+            if (InputManager.GetInstance != null)
             {
-                move = false;
-                MoveBoatFromCurrentLevelToNextLevel();
+                InputManager.GetInstance.OnEscapePressed += ZoomOut;
+                InputManager.GetInstance.OnMouseClickDown += HandleClick;
             }
         }
-
-        public void MoveBoatFromCurrentLevelToNextLevel()
+        private void OnDisable()
         {
-            var currentLevel = levelVisuals.Find(x => x.IsCurrentLevel).LevelNumber - 1;
-            var nextLevel = currentLevel + 1;
-            if (nextLevel != null)
+            if (InputManager.GetInstance != null)
             {
-                BezierPoint bp0 = bezierPoints[currentLevel];
-                BezierPoint bp1 = bezierPoints[nextLevel];
+                InputManager.GetInstance.OnEscapePressed -= ZoomOut;
+                InputManager.GetInstance.OnMouseClickDown -= HandleClick;
+            }
+            if (boatTween != null && boatTween.IsActive())
+            {
+                boatTween.Kill();
+            }
+        }
+        private void OnDestroy()
+        {
+            if (GetInstance == this)
+            {
+                GetInstance = null;
+            }
+        }
+        private void Update()
+        {
+            if (shouldCheckClick)
+            {
+                shouldCheckClick = false;
+                if (EventSystem.current != null && EventSystem.current.IsPointerOverGameObject())
+                {
+                    // Click was on UI
+                    return;
+                }
+
+                Vector2 mousePos = Camera.main.ScreenToWorldPoint(pendingClickPosition);
+                RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero, 0f);
+
+                if (hit.collider != null && hit.collider.TryGetComponent<LevelVisual>(out var levelVisual))
+                {
+                    UIController.GetInstance.ScreenEvent(ScreenType.PowerupSelection, UIScreenEvent.Push);
+                }
+            }
+        }
+        #endregion
+
+        #region Public Methods
+        public void SetBoatInCurrentLevel()
+        {
+            // Set Boat Position to Current Level
+            int currentLevelIndex = levelVisuals.Find(x => x.IsCurrentLevel).LevelNumber - 1;
+            Transform target = levelVisuals[currentLevelIndex].transform;
+            boat.SetPositionAndRotation(levelVisuals[currentLevelIndex].transform.position + target.right * boatOffset, target.rotation);
+        }
+        public void MoveBoatFromPrevToCurrentLevel()
+        {
+            int currentLevelIndex = SaveSystem.LoadInt(StringUtils.LEVELNUMBER, IntUtils.DEFAULT_LEVEL) - 1;
+            int previousLevelIndex = currentLevelIndex - 1;
+
+            if (currentLevelIndex < bezierPoints.Count)
+            {
+                BezierPoint bp0 = bezierPoints[previousLevelIndex];
+                BezierPoint bp1 = bezierPoints[currentLevelIndex];
 
                 Vector3 p0 = bp0.anchorPoint;
                 Vector3 p1 = p0 + bp0.outTangent;
@@ -134,39 +160,23 @@ namespace BeachHero
                 Vector3 p3 = bp1.anchorPoint;
 
                 float time = 0;
-                DOTween.To(() => time,
-                    x =>
-                        {
-                            time = x;
-                            Vector3 pos = BezierCurveUtils.GetPoint(p0, p1, p2, p3, time);
-                            Vector3 forward = BezierCurveUtils.GetTangent(p0, p1, p2, p3, time).normalized;
-                            boat.up = forward;
-                            boat.position = pos;
-                        },
+                boatTween = DOTween.To(
+                    () => time, x =>
+                    {
+                        time = x;
+                        Vector3 pos = BezierCurveUtils.GetPoint(p0, p1, p2, p3, time);
+                        Vector3 forward = BezierCurveUtils.GetTangent(p0, p1, p2, p3, time).normalized;
+                        boat.up = forward;
+                        Vector3 right = Vector3.Cross(forward, Vector3.forward).normalized;
+                        boat.position = pos + right * boatOffset;
+                    },
                     1,
-                    2f).SetEase(Ease.Linear);
+                    boatDuration).SetEase(Ease.InQuad);
             }
         }
-
-        private void ZoomToggle(bool value)
+        public void ZoomIn()
         {
-            if (value)
-            {
-                ZoomOut();
-            }
-            else
-            {
-                ZoomIn(levelVisuals.Find(x => x.IsCurrentLevel).WorldPosition);
-            }
-        }
-
-        private void OnDisable()
-        {
-            inputManager.OnEscapePressed -= ZoomOut;
-        }
-
-        public void ZoomIn(Vector2 position)
-        {
+            Vector2 position = levelVisuals.Find(x => x.IsCurrentLevel).WorldPosition;
             zoomOutCam.SetActive(false);
             zoomInCam.SetActive(true);
             zoomInCam.transform.position = new Vector3(position.x, position.y, zoomInCam.transform.position.z);
@@ -174,14 +184,20 @@ namespace BeachHero
             DOTween.To(() => pathLine.endWidth, (x) => pathLine.endWidth = x, zoomInThick, 1.5f);
             DOTween.To(() => pathLine.textureScale, (x) => pathLine.textureScale = x, zoomInTextureScale, 1.5f);
         }
-
-        private void ZoomOut()
+        public void ZoomOut()
         {
             zoomOutCam.SetActive(true);
             zoomInCam.SetActive(false);
             DOTween.To(() => pathLine.startWidth, (x) => pathLine.startWidth = x, zoomOutThick, 1.5f);
             DOTween.To(() => pathLine.endWidth, (x) => pathLine.endWidth = x, zoomOutThick, 1.5f);
             DOTween.To(() => pathLine.textureScale, (x) => pathLine.textureScale = x, originalTextureScale, 1.5f);
+        }
+        #endregion
+
+        private void HandleClick(Vector2 screenPosition)
+        {
+            pendingClickPosition = screenPosition;
+            shouldCheckClick = true;
         }
     }
 }
